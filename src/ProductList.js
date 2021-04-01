45ea9b7d4793ad422015a2056905520673822ab6
mandeep <rrmandeep18@gmail.com>
2021-04-01 15:34:23 +0530

    first commit

---
+++ src/ProductList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class ProductList extends React.Component {
+  constructor() {
+    super()  
+  }
+  
+  addToCart() {
+    alert("Product added to cart")
+  }
+
+  render() {
+    return (
+      <div className="card" style={{width: "25rem", marginLeft: "20px", marginTop: "5px"}}>
+          <img className="card-img-top" src={this.props.product.image_url} alt="Card image cap"></img>
+          <div className="card-body">
+              <h5 className="card-title">{this.props.product.name}</h5>
+              <p className="card-text">{this.props.product.description}</p>
+              <p className="card-text">{this.props.product.price}</p>
+              <a href="#" className="btn btn-primary" onClick={this.addToCart}>Add To Cart</a>
+          </div>
+      </div>
+    )
+  }
+}
+
+
+export default ProductList
